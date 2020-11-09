ea508fce504a1899355be7c1154fc1a54b6cddc7
erifo <eric.forsberg.work@gmail.com>
2020-11-09 18:26:57 +0100

    Finalizing
    Simplified wall initialization.
    Changed to more robust wall handling.
    Added maze entry and exit.
    Removed old ASCII display method.

--- maze.py
+++ maze.py
@@ -9,26 +9,18 @@ class Cell():
         self.visitorID = 0
 
     def initWalls(self):
-        walls = {}
-        directions = [-1, 0, 1]
-        for yMod in directions:
-            walls[yMod] = {}
-            for xMod in directions:
-                if (yMod == 0 and xMod == 0): #This is not a direction away from cell.
-                    continue
-                if (yMod != 0 and xMod != 0): #Diagonals are not valid directions.
-                    continue
-                walls[yMod][xMod] = True
+        walls = [(-1,0), (0,1), (1,0), (0,-1)]
         return walls
     
     def isWallAtMod(self, y, x):
-        return self.walls[y][x]
+        if ((y,x) in self.walls):
+            return True
     
     def razeWallAtMod(self, y, x):
-        if (self.walls[y][x] == False):
+        if (not self.isWallAtMod(y, x)):
             print("ERROR: Wall", y, x, "in cell", self.y, x, "already razed!")
         else:
-            self.walls[y][x] = False
+            self.walls = [wall for wall in self.walls if wall != (y,x)]
 
 class Maze():
     def __init__(self, height, width):
@@ -42,7 +34,7 @@ class Maze():
         cells = []
         for y in range(self.HEIGHT):
             for x in range(self.WIDTH):
-                cells.append(Cell(y, x))
+                cells.append(Cell(y, x))        
         return cells
 
     def isCellAt(self, y, x):
@@ -66,7 +58,6 @@ class Maze():
                 continue
             c2 = self.getCellAt(cell.y+d[0], cell.x+d[1])
             payload.append(c2)
-
         return payload
 
     def linkCell(self, y, x):
@@ -82,34 +73,36 @@ class Maze():
         if (len(candidateCells) == 0):
             return
 
-        #Link this cell to the found one.
+        #Link this cell to the found one by razing inbetween walls.
         nextCell = choice(candidateCells)
         currentCell.razeWallAtMod(nextCell.y-y, nextCell.x-x)
         nextCell.razeWallAtMod((nextCell.y-y)*-1, (nextCell.x-x)*-1)
-        self.linkCell(nextCell.y, nextCell.x)
 
         #Marking the cell with number indicating when it was visited.
-        currentCell.visitorID = self.debugTracer * (255 // (self.WIDTH*self.HEIGHT))
+        currentCell.visitorID = self.debugTracer * max(1,(255 // (self.WIDTH*self.HEIGHT)))
         self.debugTracer += 1
 
-        #Restarting process for next cell.
+        #Restarting process for next cell, twice.
+        #This will cause depth-first branching paths to generate.
         self.linkCell(nextCell.y, nextCell.x)
+        self.linkCell(nextCell.y, nextCell.x)
+
+    def createOpenings(self):
+        #Top
+        y = 0
+        x = randint(0, self.WIDTH-1)
+        cell = self.getCellAt(y, x)
+        cell.razeWallAtMod(-1,0)
+        #Bottom
+        y = self.HEIGHT-1
+        x = randint(0, self.WIDTH-1)
+        cell = self.getCellAt(y, x)
+        cell.razeWallAtMod(1,0)
+
 
     def linkMaze(self):
         self.debugTracer = 0
         startY = randint(0, self.HEIGHT-1)
         startX = randint(0, self.WIDTH-1)
         self.linkCell(startY, startX)
-
-    def printMaze(self):
-        payload = '╔' + '═'*self.WIDTH + '╗\n'
-        for y in range(self.HEIGHT*2):
-            payload += '║'
-            for x in range(self.WIDTH):
-                #cell = self.getCellAt(y, x)
-                payload += ' #'
-            payload += ' ║\n'
-            if (y%2 == 0):
-                payload += '║' + '#'*self.WIDTH*2 + '║\n'
-        payload += '╚' + '═'*self.WIDTH + '╝'
-        print(payload)
+        self.createOpenings()
