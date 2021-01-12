cb99978f8524616c2bf7b82444520b8a4b095c43
erifo <eric.forsberg.work@gmail.com>
2021-01-12 01:55:59 +0100

    Separated logic and rendering!

--- main.py
+++ main.py
@@ -1,78 +1,32 @@
 import sys, pygame
 from maze import Maze
-
-#Measurements in pixels.
-MAZE_HEIGHT = 30
-MAZE_WIDTH = 30
-CELL_SIZE = 20
-CELL_COLOR = (0,0,0)
-WALL_WIDTH = 1
-WALL_COLOR = (255,255,255)
+from gfx import GFX
     
-def drawCell(maze, screen, originY, originX):
-    for cell in maze.cells:
-        y = originY + (cell.y * CELL_SIZE)
-        x = originX + (cell.x * CELL_SIZE)
-        height =  CELL_SIZE
-        width = CELL_SIZE
-        pygame.draw.rect(screen, CELL_COLOR, [x, y, width, height])
+#Squares.
+MAZE_HEIGHT = 15
+MAZE_WIDTH = 15
 
-def drawWalls(maze, screen, originY, originX):
-    for cell in maze.cells:
-        y = originY + (cell.y * CELL_SIZE)
-        x = originX + (cell.x * CELL_SIZE)
-        height =  CELL_SIZE
-        width = CELL_SIZE
-        linesToDraw = []
-        if cell.isWallAtMod(0,1): #Right
-            linesToDraw.append([(x+width, y), (x+width, y+height)])
-        if cell.isWallAtMod(0,-1): #Left
-            linesToDraw.append([(x, y), (x, y+height)])
-        if cell.isWallAtMod(1,0): #Down
-            linesToDraw.append([(x, y+height), (x+width, y+height)])
-        if cell.isWallAtMod(-1,0): #Up
-            linesToDraw.append([(x, y), (x+width, y)])
-        for line in linesToDraw:
-            pygame.draw.line(screen, WALL_COLOR, line[0], line[1], width=WALL_WIDTH)
+#Pixels
+ORIGIN_Y = 5
+ORIGIN_X = 5
 
-def drawVisitorID(maze, font, screen, originY, originX):
-    pygame.font.init()
-    font = pygame.font.SysFont('Comic Sans MS', 12)
-    for cell in maze.cells:
-        y = originY + (cell.y * CELL_SIZE)+10
-        x = originX + (cell.x * CELL_SIZE)+10
-        textSurface = font.render(str(cell.visitorID), False, (255, 255, 255))
-        screen.blit(textSurface, (x, y))
-
-
-def drawMaze(maze, screen, originY, originX):
-        #Draw Background
-        black = (0,0,0)
-        screen.fill(black)
-        drawCell(maze, screen, originY, originX)
-        drawWalls(maze, screen, originY, originX)
-        #drawVisitorID(maze, font, screen, originY, originX)
-        pygame.display.flip()
-
-def drawNewMaze(screen):
-    maze = Maze(MAZE_HEIGHT, MAZE_WIDTH)
-    drawMaze(maze, screen, 5, 5)
+def showNewMaze():
+    m = Maze(MAZE_HEIGHT, MAZE_WIDTH)
+    gfx = GFX(m)
+    gfx.drawMaze(ORIGIN_Y, ORIGIN_X)
 
 def main():
-    #Setup
     pygame.init()
     pygame.display.set_caption("Maze Constructor")
-    screenSize = (MAZE_WIDTH*CELL_SIZE)+10, (MAZE_HEIGHT*CELL_SIZE)+10 #tuple
-    screen = pygame.display.set_mode(screenSize)
     
-    drawNewMaze(screen)
+    showNewMaze()
 
     while (True):
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 sys.exit()
             elif (event.type == pygame.MOUSEBUTTONDOWN):
-                drawNewMaze(screen)
+                showNewMaze()
 
 if __name__ == "__main__":
     main()
